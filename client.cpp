afac2f7d5d59de3bb906404b9de9bd5e6722ce9a
Dmitriy Korotkov <Nupellot@yahoo.com>
2021-12-09 00:31:53 +0300

    Незначительные изменения

--- client.cpp
+++ client.cpp
@@ -10,31 +10,32 @@ struct Ball {
     std::string color;
 };
 
-sf::Packet& operator <<(sf::Packet& packet, const Ball& ball)
+sf::Packet& operator << (sf::Packet& packet, const Ball& ball)
 {
     return packet << ball.x << ball.y << ball.color;
 }
 
-sf::Packet& operator >>(sf::Packet& packet, Ball& ball)
+sf::Packet& operator >> (sf::Packet& packet, Ball& ball)
 {
     return packet >> ball.x >> ball.y >> ball.color;
 }
 
+
+
 int main() {
     setlocale(LC_ALL, "");
-    sf::TcpSocket socket;
-    socket.connect("127.0.0.1", 3000); // подключаемся к серверу по заданному порту
-    sf::Packet packet; // создаём пакет для общения клиента с сервером
-
-    // инициализируем начальное положение объектов на карте, принимая данные от сервера
-    std::vector<Ball> user_balls(2);
-    for (auto &ball : user_balls) {
-        socket.receive(packet);
-        packet >> ball;
+    
+    sf::TcpSocket socket;  // Создаем сокет. У каждого клиента - свой сокет.
+    socket.connect("127.0.0.1", 3000);  // Подключаемся к серверу по заданному порту.
+    
+    sf::Packet packet;  // Создаём пакет для общения клиента с сервером.
+    std::vector<Ball> balls(2);  // Инициализируем начальное положение объектов на карте, принимая данные от сервера.
+    for (auto &ball : balls) {  // Пробегаем по всем шарам. На 1 шар 1 пакет.
+        socket.receive(packet);  // Получаем пакет.
+        packet >> ball;  // Записываем данные из пакета в текущую структуру шара.
         packet.clear();
 
-        // каждый клиент печатает информацию о пришедших к нему мячиках
-        std::cout << ball.x << ' ' << ball.y << ' ' << ball.color << '\n';
+        std::cout << ball.x << ' ' << ball.y << ' ' << ball.color << '\n';  // Дебаг.
     }
 
     // отрисуем окно c белым цветом
@@ -43,7 +44,7 @@ int main() {
 
     // отрисуем мячик с начальными координатами
     sf::CircleShape circle;
-    for (auto &ball : user_balls) {
+    for (auto &ball : balls) {
         circle.setPosition(ball.x, ball.y);
         circle.setRadius(15.f);
         circle.setFillColor(sf::Color::Black);
@@ -80,17 +81,17 @@ int main() {
         packet << dir;
         socket.send(packet);
         packet.clear();
-        std::cout << dir << '\n';
+        std::cout << dir << '\n';  // Дебаг
 
         // Отрисовываем все мячи у каждого пользователя
         // получаем обработанные(обновлённые) данные с сервера
         window.clear(sf::Color::White);
-        for (int i = 0; i < user_balls.size(); ++i) {
+        for (int i = 0; i < balls.size(); ++i) {
             socket.receive(packet);
-            packet >> user_balls[i];
+            packet >> balls[i];
             packet.clear();
-            std::cout << user_balls[i].x << ' '<< user_balls[i].y << ' ' << user_balls[i].color << '\n';
-            circle.setPosition(user_balls[i].x, user_balls[i].y);
+            std::cout << balls[i].x << ' '<< balls[i].y << ' ' << balls[i].color << '\n';
+            circle.setPosition(balls[i].x, balls[i].y);
             window.draw(circle);
         }
         window.display();
--- server.cpp
+++ server.cpp
@@ -20,74 +20,68 @@ sf::Packet& operator >>(sf::Packet& packet, Ball& ball)
     return packet >> ball.x >> ball.y >> ball.color;
 }
 
+
+
 int main(int argc, char* argv[]) {
     sf::TcpListener listener;
-
-    // устанвливаем по какому порту  будет проходить подключение к серверу
-    if (listener.listen(3000) != sf::Socket::Done) {
+    // Устанавливаем по какому порту будет проходить подключение к серверу
+    if (listener.listen(3000) != sf::Socket::Done) {  // Слушаем порт 3000.
         std::cerr << "Error";
     }
-
-    // рассмотрим случай для двух клиентов
+    // Рассмотрим случай для двух клиентов
     std::vector<sf::TcpSocket> clients(2);
-
-    // инициализация сокета для дальнейшего взаимодействия с клиентом
     // если кто-то подключается, то accept индентифицирует клиента для дальнешей работы с ним
     for (auto &socket : clients) {
         listener.accept(socket);
     }
 
-    // Для передачи даннных между клиент сервером создаём пакет, который будет летать по сети
-    sf::Packet packet;
 
-    // инициализируем клиентов значниями по умолчанию
-    std::vector<Ball> user_balls(clients.size(),{0,0, "Black"});
-
-    // записывем данные о каждом мяче на карте в пакет и отправляем каждому клиенту все мячи
-    for (int i = 0; i < clients.size(); ++i) {
-        for (int j = 0; j < user_balls.size(); ++j) {
-            packet << user_balls[j];
-            clients[i].send(packet);
+    sf::Packet packet;  // Для передачи даннных между клиент сервером создаём пакет, который будет летать по сети.
+    std::vector<Ball> balls(clients.size(),{0,0, "Black"}); // инициализируем клиентов значниями по умолчанию.
+    // Заранее отправляем клиентам данные о том, что мячи расположены на нулевых координатах
+    for (auto & client : clients) {  // Для каждого клиента.
+        for (auto & ball : balls) {  // О каждом шаре.
+            packet << ball;
+            client.send(packet);
             packet.clear();
-
-            // печатаем мячики, которые отправляюися каждому клиенту
-            std::cout << user_balls[j].x << ' '<< user_balls[j].y << ' ' << user_balls[j].color << '\n';
+            
+            std::cout << ball.x << ' '<< ball.y << ' ' << ball.color << '\n';  // Дебаг
         }
     }
 
-    while(1) {
-        // получаем пакет с информацией о перемещение какждого клиента и извлекаем информацию о перемещение каждого клиента
+    while (true) {
+        // получаем пакет с информацией о перемещении какждого клиента и извлекаем информацию о перемещение каждого клиента
         // перемещение i-ого клиента значит перемещение i-ого мячика
-        for (int i = 0; i < clients.size(); ++i) {// проблема в этом цикле - все мячи смещаются на одно и тоже расстояние одновременно
+        for (int i = 0; i < clients.size(); ++i) {
             // указывем направление движения объекта
-            std::string dir = "\0";
             clients[i].receive(packet);
+            std::string dir;
             packet >> dir;
             packet.clear();
-            std::cout << dir << "\n";
+            std::cout << dir << "\n";  // Дебаг.
 
             // обрабатываем действие пользователя
             if (dir == "UP") {
-                --user_balls[i].y;
+                --balls[i].y;
             }
             if (dir == "RIGHT") {
-                ++user_balls[i].x;
+                ++balls[i].x;
             }
             if (dir == "DOWN") {
-                ++user_balls[i].y;
+                ++balls[i].y;
             }
             if (dir == "LEFT") {
-                --user_balls[i].x;
+                --balls[i].x;
             }
         }
 
         // отправляем обновлённые данные об изменение всех объектов на сервере каждому клиенту
         for (int i = 0; i < clients.size(); ++i) {
-            for (int j = 0; j < user_balls.size(); ++j) {
-                packet << user_balls[j];
+            for (int j = 0; j < balls.size(); ++j) {
+                packet << balls[j];
                 clients[i].send(packet);
                 packet.clear();
-                std::cout << user_balls[j].x << ' '<< user_balls[j].y << ' ' << user_balls[j].color << '\n';
+                std::cout << balls[j].x << ' '<< balls[j].y << ' ' << balls[j].color << '\n';
             }
         }
     }
